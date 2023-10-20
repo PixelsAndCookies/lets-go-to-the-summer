0db558aa87674ff8ecea9128c82e5faecbb8aa91
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-10-20 14:56:36 +0200

    adding react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+
+function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    )
+}
 
 export default App
