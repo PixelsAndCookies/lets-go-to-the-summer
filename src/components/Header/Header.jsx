37039efa73e2d6344973d0470ac9cde4b5934134
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-10-31 16:14:07 +0100

    Update Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/logo/Logo_sans_fond.svg'
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="" alt="" />
+            <img src={logo} alt="Logo Summer" />
+        </header>
+    )
+}
