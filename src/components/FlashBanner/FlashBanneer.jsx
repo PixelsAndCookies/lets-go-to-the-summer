2b8464b49664402ae5a85b22f6f57eccd9c25c0c
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-10-31 16:14:33 +0100

    create flashinfo banner

---
+++ src/components/FlashBanner/FlashBanneer.jsx
@@ -0,0 +1,13 @@
+export const FlashBanner = () => {
+    return (
+        <div>
+            <h2 className="flashinfo-categ">FLASH INFO : {categ}</h2>
+            <p className="flashinfo-text">lala</p>
+        </div>
+    )
+}
+
+const categ = () => {
+    
+}
+ 
