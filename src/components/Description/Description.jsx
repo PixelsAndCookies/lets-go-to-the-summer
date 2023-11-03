5c56152a043e308f90c27ef51870d63ef6fa183e
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-03 12:24:48 +0100

    Create Description.jsx

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,14 @@
+export const Description = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. laboris nisi ut aliquip.. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur.
+            </p>
+        </div>
+    )
+}
