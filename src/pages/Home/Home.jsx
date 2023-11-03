d030d9327728c32926fed8be6156ed474e1208dd
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-03 16:08:54 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Counter } from '../../components/Counter/Counter'
 import { Description } from '../../components/Description/Description'
 import { BreakingNews } from '../../components/BreakingNews/BreakingNews'
+import { Cards } from '../../components/Cards/Cards'
 
 export const Home = () => {
     return (
@@ -11,6 +12,7 @@ export const Home = () => {
             </h1>
             <Description />
             <Counter />
+            <Cards />
         </main>
     )
 }
