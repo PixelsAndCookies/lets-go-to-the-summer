785a746ed01943a7a7b380e7f0232fd7ebdbb11d
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-03 12:25:51 +0100

    Create Counter.jsx

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,10 @@
+import counterLogo from '../../assets/icons/hamac.png'
+
+export const Counter = () => {
+    return (
+        <div className="counter-container">
+            <img src={counterLogo} alt="" />
+            <p>J-300</p>
+        </div>
+    )
+}
