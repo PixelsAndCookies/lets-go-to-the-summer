fd7eae95ff113a996b5f1dd1c79358f5b56bfa0b
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-03 12:24:32 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import logo from '../../assets/logo/Logo_sans_fond.svg'
 export const Header = () => {
     return (
         <header>
-            <img src="" alt="" />
             <img src={logo} alt="Logo Summer" />
         </header>
     )
