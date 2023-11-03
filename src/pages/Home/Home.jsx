a446dde8fa524257496d9504d6acfc4e0b33a024
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-03 12:00:17 +0100

    Adding h1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,9 @@ export const Home = () => {
     return (
         <main>
             <FlashBanner />
+            <h1>
+                Souvenirs d&apos;été <br /><span>Les aventures fun-tastiques en famille</span>
+            </h1>
         </main>
     )
 }
