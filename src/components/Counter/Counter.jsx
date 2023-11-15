da2d0b5f03a128f603bee7525893a7452c41cf82
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-15 16:50:22 +0100

    creating counter to deadline date

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react'
 import counterLogo from '../../assets/icons/hamac.png'
 
 export const Counter = () => {
+    const [timeLeft, setTimeLeft] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 })
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const deadlineDate = new Date('2024-07-13')
+            const currentDate = new Date()
+            const timeRemaining = Math.max(0, deadlineDate - currentDate)
+
+            const days = Math.floor(timeRemaining / (24 * 60 * 60 * 1000))
+            const hours = Math.floor((timeRemaining % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000))
+            const minutes = Math.floor((timeRemaining % (60 * 60 * 1000)) / (60 * 1000))
+            const seconds = Math.floor((timeRemaining % (60 * 1000)) / 1000)
+
+            setTimeLeft({ days, hours, minutes, seconds })
+        }, 1000)
+        return () => clearInterval(interval)
+    }, [])
     return (
         <div className="counter-container">
             <img src={counterLogo} alt="" />
-            <p>J-300</p>
+            <p>
+                J-{timeLeft.days}d {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
+            </p>
         </div>
     )
 }
