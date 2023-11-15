c336520e2714a07c6566088dfd9d2d914fb2643b
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-15 16:25:38 +0100

    adding useState and useEffect

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,16 +1,27 @@
+import { useState, useEffect } from 'react'
 import breakingNews from '../../data/breaking-news'
 
 export const BreakingNews = () => {
+    const [selectedId, setSelectedId] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSelectedId((prevId) => (prevId + 1) % breakingNews.length)
+        }, 7500)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
     return (
         <div>
-            {breakingNews.map((text, id) => {
-                return (
-                    <div className="news-container" key={id}>
-                        <p className="news-categ">BREAKING NEWS : {text.title}</p>
-                        <p className="news-text">{text.content}</p>
-                    </div>
-                )
-            })}
+            {breakingNews.map((news) => (
+                <div key={news.id} className={`news-container ${news.id === selectedId ? 'visible' : 'hidden'}`}>
+                    <p className="news-categ">BREAKING NEWS : {news.title}</p>
+                    <p className="news-text">{news.content}</p>
+                </div>
+            ))}
         </div>
     )
 }
