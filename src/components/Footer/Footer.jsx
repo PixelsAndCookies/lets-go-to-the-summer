694bca124a2368e535e5dcd97a067fd59ddce89d
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-15 12:11:13 +0100

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <h3>Lorem Ipsum</h3>
+                <li>dolor sit amet</li>
+                <li>consectetur adipiscing elit</li>
+                <li>sed do eiusmod</li>
+            </ul>
+            <ul> 
+                <h3>Cicero Finibus </h3>
+                <li>tempor incididunt</li>
+                <li>labore et dolore</li>
+                <li>magna aliqua</li>
+            </ul>
+            <ul>
+                <h3>Bonorum Malorum</h3>
+                <li>Ut enim ad minim veniam</li>
+                <li>quis nostrud exercitation</li>
+                <li>ullamco laboris</li>
+            </ul>
+        </footer>
+    )
+}
