00092676b7ebc5eb199e86a65dc6a77e67ab0ad8
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-21 14:02:27 +0100

    Delete description component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import { Counter } from '../../components/Counter/Counter'
-import { Description } from '../../components/Description/Description'
 import { BreakingNews } from '../../components/BreakingNews/BreakingNews'
 import { Cards } from '../../components/Cards/Cards'
 
@@ -8,9 +7,9 @@ export const Home = () => {
         <main>
             <BreakingNews />
             <h1>
-                Souvenirs d&apos;été <br /><span>Les aventures fun-tastiques en famille</span>
+                Souvenirs d&apos;été <br />
+                <span>Les aventures fun-tastiques en famille</span>
             </h1>
-            <Description />
             <Counter />
             <Cards />
         </main>
