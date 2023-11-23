691310e422f5b22b78dbb41091f7874b3dbc4c0c
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-23 17:09:45 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,12 +6,14 @@ export const Home = () => {
     return (
         <main>
             <BreakingNews />
-            <h1>
-                Souvenirs d&apos;été <br />
-                <span>Les aventures fun-tastiques en famille</span>
-            </h1>
-            <Counter />
-            <Cards />
+            <div className='main-container'>
+                <h1>
+                    Souvenirs d&apos;été <br />
+                    <span>Les aventures fun-tastiques en famille</span>
+                </h1>
+                <Counter />
+                <Cards />
+            </div>
         </main>
     )
 }
