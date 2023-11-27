3888f7da7930e25d62c886e355d8e91d19bbe346
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-27 18:16:54 +0100

    Adding header and Logo component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,25 @@
+import { Logo } from '../../components/Logo/Logo'
 import { Counter } from '../../components/Counter/Counter'
 import { BreakingNews } from '../../components/BreakingNews/BreakingNews'
 import { Cards } from '../../components/Cards/Cards'
 
 export const Home = () => {
     return (
-        <main>
-            <BreakingNews />
-            <div className='main-container'>
-                <h1>
-                    Souvenirs d&apos;été <br />
-                    <span>Les aventures fun-tastiques en famille</span>
-                </h1>
-                <Counter />
-                <Cards />
-            </div>
-        </main>
+        <>
+            <header>
+                <Logo />
+            </header>
+            <main>
+                <BreakingNews />
+                <div className="main-container">
+                    <h1>
+                        Souvenirs d&apos;été <br />
+                        <span>Les aventures fun-tastiques en famille</span>
+                    </h1>
+                    <Counter />
+                    <Cards />
+                </div>
+            </main>
+        </>
     )
 }
