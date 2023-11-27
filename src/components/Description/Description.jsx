5a40ac9ed627e002acf7c4eb2bde74663e22470d
mroulland <morgane.roulland@hotmail.fr>
2023-11-27 19:04:26 +0100

    Description init

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,16 +1,14 @@
 import description from "../../data/description.json";
 
 export const Description = (props) => {
-  const array = description[props.item];
-  
+
+  const item = description[props.item];
   return (
-    <section className="description">
-      {array.map((presentation) => (
-        <div key={presentation.id}>
-          <h2>{presentation.title}</h2>
-          <p>{presentation.content}</p>
+    <section>
+        <div key={item.id}>
+          <h2>{item.title}</h2>
+          <p>{item.content}</p>
         </div>
-      ))}
     </section>
   );
 };
