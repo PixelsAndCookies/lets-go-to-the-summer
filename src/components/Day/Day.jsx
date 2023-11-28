d0035c04d70a27db6d189810fdce5ff42a581158
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-11-28 18:41:59 +0100

    Update Day.jsx

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,11 +1,12 @@
 import days from '../../data/days.json'
 
-export const Day = () => {
+export const Day = () => {    
     return (
         <section>
             {days.map((day) => {
+                const articleClass = `articleDay articleDay--${day.id}`
                 return (
-                    <article key={day.id}>
+                    <article key={day.id} className={articleClass}>
                         <h2>{day.title}</h2>
                         <p></p>
                         <img src="" alt="" />
