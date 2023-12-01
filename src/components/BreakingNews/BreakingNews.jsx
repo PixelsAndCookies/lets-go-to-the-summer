3c205416f07e00394be4f8e177ca71ea23ac6d94
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-01 16:54:58 +0100

    Update BreakingNews.jsx

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,32 +1,28 @@
-import { useState, useEffect } from "react";
-import breakingNews from "../../data/breaking-news";
+import { useState, useEffect } from 'react'
+import breakingNews from '../../data/breaking-news'
 
 export const BreakingNews = () => {
-  const [selectedId, setSelectedId] = useState(0);
+    const [selectedId, setSelectedId] = useState(0)
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSelectedId((prevId) => (prevId + 1) % breakingNews.length);
-    }, 7500);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSelectedId((prevId) => (prevId + 1) % breakingNews.length)
+        }, 7500)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div>
-      {breakingNews.map((news) => (
-        <div
-          key={news.id}
-          className={`news-container ${
-            news.id === selectedId ? "visible" : "hidden"
-          }`}
-        >
-          <p className="news-categ">BREAKING NEWS : {news.title}</p>
-          <p className="news-text">{news.content}</p>
+    return (
+        <div>
+            {breakingNews.map((news) => (
+                <div
+                    key={news.id}
+                    className={`news-container ${news.id === selectedId ? 'visible' : 'hidden'}`}>
+                    <p className="news-categ">BREAKING NEWS : {news.title}</p>
+                    <p className="news-text">{news.content}</p>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
-};
+    )
+}
