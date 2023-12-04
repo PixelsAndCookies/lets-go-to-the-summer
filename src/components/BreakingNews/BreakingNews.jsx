10ceabd3c28535e6387440de509e8fdcd51d28f3
LaurineRoulland <l.roulland@hotmail.fr>
2023-12-04 11:13:57 +0100

    Update description

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,90 +1,40 @@
-<<<<<<< Updated upstream
-import { useState, useEffect } from 'react'
-import breakingNews from '../../data/breaking-news'
+import { useState, useEffect } from 'react';
+import breakingNews from '../../data/breaking-news';
 
 export const BreakingNews = () => {
-    const [selectedId, setSelectedId] = useState(0)
+    const [selectedId, setSelectedId] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setSelectedId((prevId) => (prevId + 1) % breakingNews.length)
-        }, 7500)
+            setSelectedId((prevId) => (prevId + 1) % breakingNews.length);
+        }, 2000);
+
         return () => {
-            clearInterval(interval)
-        }
-    }, [])
+            clearInterval(interval);
+        };
+    }, []);
 
     return (
         <div>
             {breakingNews.map((news) => (
                 <div
                     key={news.id}
-                    className={`news-container ${news.id === selectedId ? 'visible' : 'hidden'}`}>
-                    <p className="news-categ">BREAKING NEWS : {news.title}</p>
-                    <p className="news-text">{news.content}</p>
-                </div>
-            ))}
-        </div>
-    )
-}
-=======
-import breakingNews from '../../data/breaking-news';
-
-export const BreakingNews = () => {
-    return (
-        <div>
-            <p className="news-categ">BREAKING NEWS</p>
-            {breakingNews.map((news) => (
-                <div key={news.id} className="news-container news-text-bg">
-                    <p className="news-text">
-                        <strong className="news-text-title">
-                            {news.title}
-                        </strong>{' '}
-                        : {news.content}
-                    </p>
+                    className={`news-container ${
+                        news.id === selectedId ? 'visible' : 'hidden'
+                    }`}
+                >
+                    <p className="news-categ">BREAKING NEWS</p>
+                    <div className="news-text-bg">
+                        <p className="news-text">
+                            <strong className="news-text-title">
+                                {news.title}
+                            </strong>{' '}
+                            : {news.content}
+                        </p>
+                    </div>
                 </div>
             ))}
         </div>
     );
 };
 
-// import { useState, useEffect } from 'react';
-// import breakingNews from '../../data/breaking-news';
-
-// export const BreakingNews = () => {
-//     const [selectedId, setSelectedId] = useState(0);
-
-//     useEffect(() => {
-//         const interval = setInterval(() => {
-//             setSelectedId((prevId) => (prevId + 1) % breakingNews.length);
-//         }, 9500);
-
-//         return () => {
-//             clearInterval(interval);
-//         };
-//     }, []);
-
-//     return (
-//         <div>
-//             {breakingNews.map((news) => (
-//                 <div
-//                     key={news.id}
-//                     className={`news-container ${
-//                         news.id === selectedId ? 'visible' : 'hidden'
-//                     }`}
-//                 >
-//                     <p className="news-categ">BREAKING NEWS</p>
-//                     <div className="news-text-bg">
-//                         <p className="news-text">
-//                             <strong className="news-text-title">
-//                                 {news.title}
-//                             </strong>{' '}
-//                             : {news.content}
-//                         </p>
-//                     </div>
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// };
->>>>>>> Stashed changes
