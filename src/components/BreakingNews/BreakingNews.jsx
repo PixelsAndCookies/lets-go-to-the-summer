abbb804d9fa2d062df6257bfdc56d5e7e0cb9372
LaurineRoulland <l.roulland@hotmail.fr>
2023-12-04 09:23:22 +0100

    Update BreakingNews.jsx
    Faire défiler  les messages comme une banderole

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState, useEffect } from 'react'
 import breakingNews from '../../data/breaking-news'
 
@@ -26,3 +27,64 @@ export const BreakingNews = () => {
         </div>
     )
 }
+=======
+import breakingNews from '../../data/breaking-news';
+
+export const BreakingNews = () => {
+    return (
+        <div>
+            <p className="news-categ">BREAKING NEWS</p>
+            {breakingNews.map((news) => (
+                <div key={news.id} className="news-container news-text-bg">
+                    <p className="news-text">
+                        <strong className="news-text-title">
+                            {news.title}
+                        </strong>{' '}
+                        : {news.content}
+                    </p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+// import { useState, useEffect } from 'react';
+// import breakingNews from '../../data/breaking-news';
+
+// export const BreakingNews = () => {
+//     const [selectedId, setSelectedId] = useState(0);
+
+//     useEffect(() => {
+//         const interval = setInterval(() => {
+//             setSelectedId((prevId) => (prevId + 1) % breakingNews.length);
+//         }, 9500);
+
+//         return () => {
+//             clearInterval(interval);
+//         };
+//     }, []);
+
+//     return (
+//         <div>
+//             {breakingNews.map((news) => (
+//                 <div
+//                     key={news.id}
+//                     className={`news-container ${
+//                         news.id === selectedId ? 'visible' : 'hidden'
+//                     }`}
+//                 >
+//                     <p className="news-categ">BREAKING NEWS</p>
+//                     <div className="news-text-bg">
+//                         <p className="news-text">
+//                             <strong className="news-text-title">
+//                                 {news.title}
+//                             </strong>{' '}
+//                             : {news.content}
+//                         </p>
+//                     </div>
+//                 </div>
+//             ))}
+//         </div>
+//     );
+// };
+>>>>>>> Stashed changes
