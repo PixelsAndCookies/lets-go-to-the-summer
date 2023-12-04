600ce5b5ab0ad242390248be567ff2f8da67ebe9
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-04 19:14:57 +0100

    Update Day.jsx

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import days from '../../data/days.json'
 
 export const Day = () => {
@@ -5,27 +6,41 @@ export const Day = () => {
         <>
             {days.map((day) => {
                 return (
-                    <article key={day.id} className={`dayCard dayCard--${day.id}`}>
-                        <h2>{day.title}</h2>
-                        {Object.keys(day.texts).map((index) => {
-                            return (
-                                <p key={index} className="text">
-                                    {typeof day.texts[index] === 'string' ? day.texts[index] : ''}
-                                </p>
-                            )
-                        })}
-                        {/*{day.contents.map((content) => {
-                            <p key={content.id}>{content.text}</p>
-                        })}*/}
-                        {day.pictures.map((picture) => (
-                            <img
-                                key={picture.id}
-                                className={`dayCard__photo dayCard--${day.id}__photo dayCard--${day.id}__photo--${picture.id}`}
-                                src={`/${picture.src}`}
-                                alt={picture.alt}
-                            />
-                        ))}
-                        <>{day.quotes !== null && <q className="quotes">{day.quotes}</q>}</>
+                    <article key={day.id} className={`dayCard`}>
+                        <h2 className={'dayCard__title'}>{day.title}</h2>
+                        <div className={`dayCard__${day.id}`}>
+                            {day.contents.map((content) => (
+                                <React.Fragment key={content.id}>
+                                    {typeof content.text === 'string' ? (
+                                        <p className={`dayCard__${day.id}__text--${content.id}`}>
+                                            {content.text}
+                                        </p>
+                                    ) : (
+                                        <div className={`dayCard__${day.id}__list`}>
+                                            <p className={`dayCard__${day.id}__list__title`}>
+                                                {content.text.name}
+                                            </p>
+                                            <ul>
+                                                {content.text.list.map((team, index) => (
+                                                    <li key={index}>{team}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    )}
+                                </React.Fragment>
+                            ))}
+                            <div className={`dayCard__${day.id}__container`}>
+                                {day.pictures.map((picture) => (
+                                    <img
+                                        key={picture.id}
+                                        className={`dayCard__${day.id}__container-img dayCard__${day.id}__container-img--${picture.id}`}
+                                        src={`/${picture.src}`}
+                                        alt={picture.alt}
+                                    />
+                                ))}
+                            </div>
+                            {day.quotes !== null && <q className="quotes">{day.quotes}</q>}
+                        </div>
                     </article>
                 )
             })}
