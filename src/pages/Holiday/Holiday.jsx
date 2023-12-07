360067043112d3ec1002fbe95157df03ff3c7a6f
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-07 17:58:06 +0100

    import all the templates and days.json directly into Holidaypage

--- src/pages/Holiday/Holiday.jsx
+++ src/pages/Holiday/Holiday.jsx
@@ -1,8 +1,17 @@
-import { Day } from "../../components/Day/Day"
-import { Logo } from "../../components/Logo/Logo"
-import { Navbar } from "../../components/Navbar/Navbar"
+import { Day } from '../../components/Day/Day'
+import { Logo } from '../../components/Logo/Logo'
+import { Navbar } from '../../components/Navbar/Navbar'
+import { Template1 } from '../../components/Templates/Template1'
+import { Template2 } from '../../components/Templates/Template2'
+import { Template3 } from '../../components/Templates/Template3'
+import { Template4 } from '../../components/Templates/Template4'
+import { Template5 } from '../../components/Templates/Template5'
+import { Template6 } from '../../components/Templates/Template6'
+import { Template7 } from '../../components/Templates/Template7'
+import days from '../../data/days.json'
 
 export const Holiday = () => {
+    const [saturday, sunday, monday, tuesday, wednesday, thursday, friday] = days
     return (
         <>
             <header className="holiday-header">
@@ -10,7 +19,27 @@ export const Holiday = () => {
                 <Navbar />
             </header>
             <main className="holiday-main">
-                <Day />
+                <Day key={saturday.id} day={saturday}>
+                    <Template1 day={saturday} />
+                </Day>
+                <Day key={sunday.id} day={sunday}>
+                    <Template2 day={sunday} />
+                </Day>
+                <Day key={monday.id} day={monday}>
+                    <Template3 day={monday} />
+                </Day>
+                <Day key={tuesday.id} day={tuesday}>
+                    <Template4 day={tuesday} />
+                </Day>
+                <Day key={wednesday.id} day={wednesday}>
+                    <Template5 day={wednesday} />
+                </Day>
+                <Day key={thursday.id} day={thursday}>
+                    <Template6 day={thursday} />
+                </Day>
+                <Day key={friday.id} day={friday}>
+                    <Template7 day={friday} />
+                </Day>                 
             </main>
         </>
     )
