769ec21d694caa8920c8192e67438678b9c74ac3
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-07 17:56:02 +0100

    Simplification of the structure

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,19 +1,10 @@
-import days from '../../data/days.json'
-import { Template1 } from '../Templates/Template1'
-import { Template2 } from '../Templates/Template2'
-
-export const Day = () => {
+export const Day = ({ day, children }) => {
     return (
         <>
-            {days.map((day) => {
-                return (
-                    <article key={day.id} className={`dayCard`}>
-                        <h2 className={'dayCard__title'}>{day.title}</h2>
-                        <Template1 />
-                        <Template2 />
-                    </article>
-                )
-            })}
+            <article key={day.id} className={`dayCard`}>
+                <h2 className={'dayCard__title'}>{day.title}</h2>                
+                {children}
+            </article>
         </>
     )
 }
