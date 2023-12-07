d93b1cc883ad3aa832fb6503f3b9b8162c0bde3d
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-07 11:17:59 +0100

    Update Day.jsx

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import days from '../../data/days.json'
+import { Template1 } from '../Templates/Template1'
+import { Template2 } from '../Templates/Template2'
 
 export const Day = () => {
     return (
@@ -8,39 +9,8 @@ export const Day = () => {
                 return (
                     <article key={day.id} className={`dayCard`}>
                         <h2 className={'dayCard__title'}>{day.title}</h2>
-                        <div className={`dayCard__${day.id}`}>
-                            {day.contents.map((content) => (
-                                <React.Fragment key={content.id}>
-                                    {typeof content.text === 'string' ? (
-                                        <p className={`dayCard__${day.id}__text--${content.id}`}>
-                                            {content.text}
-                                        </p>
-                                    ) : (
-                                        <div className={`dayCard__${day.id}__list`}>
-                                            <p className={`dayCard__${day.id}__list__title`}>
-                                                {content.text.name}
-                                            </p>
-                                            <ul>
-                                                {content.text.list.map((team, index) => (
-                                                    <li key={index}>{team}</li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    )}
-                                </React.Fragment>
-                            ))}
-                            <div className={`dayCard__${day.id}__container`}>
-                                {day.pictures.map((picture) => (
-                                    <img
-                                        key={picture.id}
-                                        className={`dayCard__${day.id}__container-img dayCard__${day.id}__container-img--${picture.id}`}
-                                        src={`/${picture.src}`}
-                                        alt={picture.alt}
-                                    />
-                                ))}
-                            </div>
-                            {day.quotes !== null && <q className="quotes">{day.quotes}</q>}
-                        </div>
+                        <Template1 />
+                        <Template2 />
                     </article>
                 )
             })}
