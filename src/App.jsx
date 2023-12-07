c68253d08b6c4cea74fbaae68e38fc703ad3cfdf
LaurineRoulland <l.roulland@hotmail.fr>
2023-12-07 14:34:49 +0100

    Adding component About
    Création du composent About pour la section "a propos"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home/Home.jsx'
-import { Holiday } from './pages/Holiday/Holiday.jsx'
-import { Footer } from './components/Footer/Footer.jsx'
-import './style/main.scss'
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Home.jsx';
+import { Holiday } from './pages/Holiday/Holiday.jsx';
+import { About } from './pages/About/About.jsx';
+import { Footer } from './components/Footer/Footer.jsx';
+import './style/main.scss';
 
 export default function App() {
     return (
@@ -10,8 +11,9 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/holidays/:id" element={<Holiday />} />
+                <Route path="/about" element={<About />} />
             </Routes>
             <Footer />
         </div>
-    )
+    );
 }
