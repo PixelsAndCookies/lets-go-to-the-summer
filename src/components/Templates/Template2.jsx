f44c078db6ab27a1428120cd35bd6bf330b6e437
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-07 17:56:29 +0100

    Update Template2.jsx

--- src/components/Templates/Template2.jsx
+++ src/components/Templates/Template2.jsx
@@ -1,2 +1,25 @@
-export const Template2 = () => {
+export const Template2 = ({ day }) => {
+    return (
+        <section key={day.id} className={`${day.id}`}>
+            <div className={`${day.id}__containerTxt`}>
+                {day.contents.map((content) => (
+                        <p key={content.id}
+                            className={`${day.id}__containerTxt__text ${day.id}__containerTxt__text--${content.id}`}>
+                            {content.text}
+                        </p>
+                ))}
+            </div>
+            <div className={`${day.id}__containerImg`}>
+                {day.pictures.map((picture) => (
+                    <img
+                        key={picture.id}
+                        className={`${day.id}__containerImg--${picture.id}`}
+                        src={`/${picture.src}`}
+                        alt={picture.alt}
+                    />
+                ))}
+            </div>
+            {day.quotes !== null && <q className={`${day.id}__quotes`}>{day.quotes}</q>}
+        </section>
+    )
 }
