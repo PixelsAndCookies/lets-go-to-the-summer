e71dfa4de70b1062067d534a210b45af084c7443
LaurineRoulland <l.roulland@hotmail.fr>
2023-12-08 14:34:59 +0100

    Update About Page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,9 +6,18 @@ export const About = () => {
             <header className="home-header">
                 <Logo />
             </header>
-            <main>
-                <article>
-                    <p></p>
+            <main className='main-container'>
+                <article className="description">
+                    <h2>A propos</h2>
+                    <p>
+                        Quelques petites informations concernant ce site.
+                        <br></br>
+                        Ce site est crée par la famille, pour la famille ! Par
+                        ailleurs, tout ce qui s'y trouve est protégé et n'est en
+                        aucun cas divulgué sur la toile. Afin de préserver notre
+                        intimité et continuer à partager nos souvenirs et nos
+                        annecdotes en toute confiance.
+                    </p>
                 </article>
             </main>
         </>
