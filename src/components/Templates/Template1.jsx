69a2a0e107290d4ccde76ccedafac38bc8acaf00
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-12 10:32:29 +0100

    Update Template1.jsx

--- src/components/Templates/Template1.jsx
+++ src/components/Templates/Template1.jsx
@@ -1,22 +1,22 @@
 export const Template1 = ({ day }) => {
     return (
-        <section key={day.id} className={`${day.id}`}>
+        <section key={day.id} className={day.id}>
             {day.contents.map((content) => (
-                    <p key={content.id} className={`${day.id}__p ${day.id}__p--${content.id}`}>
+                    <p key={content.id} className={day.id+"__p"+day.id+"__p--"+content.id}>
                         {content.text}
                     </p>
             ))}
-            <div className={`${day.id}__containerImg`}>
+            <div className={day.id+"__containerImg"}>
                 {day.pictures.map((picture) => (
                     <img
                         key={picture.id}
-                        className={`${day.id}__containerImg--${picture.id}`}
-                        src={`/${picture.src}`}
+                        className={day.id+"__containerImg--"+picture.id}
+                        src={"/"+picture.src}
                         alt={picture.alt}
                     />
                 ))}
             </div>
-            {day.quotes !== null && <q className={`${day.id}__quotes`}>{day.quotes}</q>}
+            {day.quotes !== null && <q className={day.id+"__quotes"}>{day.quotes}</q>}
         </section>
     )
 }
