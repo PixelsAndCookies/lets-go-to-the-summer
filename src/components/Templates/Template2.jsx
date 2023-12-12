a2f8db46c13519e91f73f99382c6e2be1c56c80a
mroulland <morgane.roulland@hotmail.fr>
2023-12-12 13:00:07 +0100

    Finitions template 2 + responsive

--- src/components/Templates/Template2.jsx
+++ src/components/Templates/Template2.jsx
@@ -1,18 +1,34 @@
 export const Template2 = ({ day }) => {
+    const template = "template2";
+    const sauceBarbecue = "30";
+    const sauceBearnaise = "70";
+
+
+
     return (
-        <section key={day.id} className={day.id}>
-            <div className={day.id+"__containerTxt"}>
+        <section key={template} className={template}>
+            <div className={template+"__containerTxt"}>
                 {day.contents.map((content) => (
-                        <p key={content.id}
-                            className={day.id+"__containerTxt__text "+day.id+"__containerTxt__text--"+content.id}>
-                            {content.text}
-                        </p>
+                <p key={content.id}
+                    className={template+"__containerTxt__text "+template+"__containerTxt__text--"+content.id}>
+                    {content.text}
+                </p>
                 ))}
+                <form action="#">
+                    <div>  
+                        <input type="radio" value="0" id="barbecue" name="sauce"/> <label htmlFor="barbecue">Team barbecue</label>
+                        <div className="progressBar"><div className="filler" style={{width: sauceBarbecue + '%'}}></div></div>
+                    </div>
+                    <div>
+                        <input type="radio" value="1" id="bearnaise" name="sauce"/> <label htmlFor="bearnaise">Team béarnaise</label>
+                        <div className="progressBar"><div className="filler" style={{width: sauceBearnaise + '%'}}></div></div>
+                    </div>
+                </form>
             </div>
-            <div className={day.id+"__containerImg"}>
+            <div className={template+"__containerImg"}>
                 {day.pictures.map((picture) => (
                     <figure key={picture.id}
-                        className={day.id+"__containerImg--"+picture.id}>
+                        className={template+"__containerImg--"+picture.id}>
                     <img
                         src={"/"+picture.src}
                         alt={picture.alt}
@@ -20,7 +36,7 @@ export const Template2 = ({ day }) => {
                     <figcaption>Blabla</figcaption></figure>
                 ))}
             </div>
-            {day.quotes !== null && <q className={day.id+"__quotes"}>{day.quotes}</q>}
+            {day.quotes !== null && <q className={template+"__quotes"}>{day.quotes}</q>}
         </section>
     )
 }
