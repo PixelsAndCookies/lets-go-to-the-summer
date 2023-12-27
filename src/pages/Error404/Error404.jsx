63c3562428a7d2ccf32a3be83b8fb42390fecb19
LaurineRoulland <l.roulland@hotmail.fr>
2023-12-27 15:28:29 +0100

    Update Error404

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,11 +1,13 @@
-import { Logo } from '../../components/Logo/Logo';
-
 export const Error404 = () => {
     return (
         <>
             <main className="main-error home-header">
                 <section className="section-error">
-                    <p>Oupsi ?</p>
+                    <h3>Oupsi ?</h3>
+                    <p>
+                        La page que vous recherchez n'est actuellement pas
+                        disponible.{' '}
+                    </p>
                 </section>
             </main>
         </>
