7e2f20d415f3210f796751e5b9593b9b428c6492
LaurineRoulland <l.roulland@hotmail.fr>
2023-12-27 14:19:42 +0100

    Update Template6.jsx

--- src/components/Templates/Template6.jsx
+++ src/components/Templates/Template6.jsx
@@ -1,17 +1,19 @@
 export const Template6 = ({ day }) => {
     const template = 'template6';
-    const firstPicture = day.picture.lengh > 0 ? day.picture[0] : null;
+    console.log(day);
+
+    const firstPicture = day.pictures.length > 0 ? day.pictures[0] : null;
 
     return (
         <section key={day.id} className={template}>
-             <div className="first_picture">
+            <div className="first_picture">
                 {firstPicture && (
                     <img
                         src={'/' + firstPicture.src}
                         alt={firstPicture.alt}
                         className="picture0"
                     />
-                )} 
+                )}
             </div>
             <div className="container">
                 {day.pictures.map((picture, index) => (
