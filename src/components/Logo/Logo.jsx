2b674f02df7486c6aacfabef4145a29d3222a76b
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-29 14:21:50 +0100

    Deleting logo, and changing " ' " to " &apos; "

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom'
-import logo from '../../assets/logo/Logo_sans_fond.svg'
 
 export const Logo = () => {
     return (
@@ -10,7 +9,7 @@ export const Logo = () => {
                     <span className="warp__0">L</span>
                     <span className="warp__1">e</span>
                     <span className="warp__2">t</span>
-                    <span className="warp__3">'</span>
+                    <span className="warp__3">&apos;</span>
                     <span className="warp__4">s</span>
                     <span className="warp__5"> </span>
                     <span className="warp__6">g</span>
