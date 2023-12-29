fcaf3f1003a79adb0915cb37eb2f8187c465caed
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-29 14:21:17 +0100

    Changing " ' " to " &apos; "

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -13,7 +13,7 @@ export const About = () => {
                         Quelques petites informations concernant ce site.
                         <br></br>
                         Ce site est crée par la famille, pour la famille ! Par
-                        ailleurs, tout ce qui s'y trouve est protégé et n'est en
+                        ailleurs, tout ce qui s&apos;y trouve est protégé et n&apos;est en
                         aucun cas divulgué sur la toile. Afin de préserver notre
                         intimité et continuer à partager nos souvenirs et nos
                         annecdotes en toute confiance.
