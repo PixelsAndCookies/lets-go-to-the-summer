e2f6c567eb7465f9ae4ba380ecbe835e8608220d
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-29 14:23:32 +0100

    Changing structure and className, adding carousel background

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -42,39 +42,45 @@ export const Carousel = ({ slides }) => {
                 setAutoPlay(true)
             }}
         >
-            {slides.map((picture, index) => {
-                return (
-                    <img
-                        src={'/' + picture.src}
-                        alt=""
-                        key={picture.id}
-                        className={
-                            index === currentSlide ? 'carousel__visible' : 'carousel__invisible'
-                        }
-                    />
-                )
-            })}
-            <button className="btn carousel__arrow carousel__arrow--left" onClick={slideLeft}>
-                &lt; {/* Flèche gauche */}
-            </button>
-            <button
-                className="btn carousel__arrow carousel__arrow--right"
-                onClick={memoizedSlideRight}
-            >
-                &gt; {/* Flèche droite */}
-            </button>
-            <div className="carousel__dots">
-                {slides.map((_, index) => (
-                    <span
-                        key={index}
-                        className={
-                            index === currentSlide
-                                ? 'carousel__dots__dot carousel__dots__dot--active'
-                                : 'carousel__dots__dot'
-                        }
-                        onClick={() => setCurrentSlide(index)}
-                    ></span>
+            <div className="carousel__container">
+                <button className="btn carousel__arrow carousel__arrow--left" onClick={slideLeft}>
+                    &lt; {/* Flèche gauche */}
+                </button>
+                {slides.map((picture, index) => (
+                    <div className="container" key={picture.id}>
+                        <img
+                            src={'/' + picture.src}
+                            alt=""
+                            className={
+                                index === currentSlide
+                                    ? 'container__image container__image--visible'
+                                    : 'container__image container__image--invisible'
+                            }
+                        />
+                        {index === currentSlide && (
+                            <img src={'/' + picture.src} alt="" className="container__background" />
+                        )}
+                    </div>
                 ))}
+                <button
+                    className="btn carousel__arrow carousel__arrow--right"
+                    onClick={memoizedSlideRight}
+                >
+                    &gt; {/* Flèche droite */}
+                </button>
+                <div className="carousel__dots">
+                    {slides.map((_, index) => (
+                        <span
+                            key={index}
+                            className={
+                                index === currentSlide
+                                    ? 'carousel__dots__dot carousel__dots__dot--active'
+                                    : 'carousel__dots__dot'
+                            }
+                            onClick={() => setCurrentSlide(index)}
+                        ></span>
+                    ))}
+                </div>
             </div>
         </div>
     )
