992968c1109fcd7964c1a26d79edcce63c362b12
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-30 13:39:11 +0100

    Adding Contact Link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import avatargroup from '/pictures/avatars_group_cut.png';
-import { Link } from 'react-router-dom';
+import avatargroup from '/pictures/avatars_group_cut.png'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
     return (
@@ -15,8 +15,11 @@ export const Footer = () => {
                 <li>
                     <Link to="/licences">Licences</Link>
                 </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
                 <li>Made by Studio Balignon-an</li>
             </ul>
         </footer>
-    );
-};
+    )
+}
