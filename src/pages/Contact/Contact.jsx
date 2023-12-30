aec7485778908e66a0509c5dbcf14352e1ee878a
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2023-12-30 13:39:01 +0100

    Create Contact.jsx

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,16 @@
+import { Logo } from '../../components/Logo/Logo'
+
+export const Contact = () => {
+    return (
+        <>
+            <header className="home-header">
+                <Logo />
+            </header>
+            <main className="main-container">
+                <article className="description">
+                    <h2>Contact</h2>
+                </article>
+            </main>
+        </>
+    )
+}
