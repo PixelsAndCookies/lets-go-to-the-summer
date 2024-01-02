6e30f24ab81f27da2fe4c534350cb35910ae9067
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-02 14:38:42 +0100

    Deleting Day component and scss

--- src/components/Day/Day.jsx
+++
@@ -1,8 +0,0 @@
-export const Day = ({ day, children }) => {
-    return (
-        <article key={day.id} className={'dayCard '+day.template}>
-            <h2 className={'dayCard__title'}>{day.title}</h2>
-            {children}
-        </article>
-    )
-}
