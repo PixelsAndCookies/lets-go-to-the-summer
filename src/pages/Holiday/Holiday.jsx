d770f9d4da37e7e5b241f09844dadeda1dc11e31
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-02 14:38:13 +0100

    Adding Day content directly inside holiday-main

--- src/pages/Holiday/Holiday.jsx
+++ src/pages/Holiday/Holiday.jsx
@@ -3,7 +3,6 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { Error404 } from '../Error404/Error404'
 import { Logo } from '../../components/Logo/Logo'
 import { Navbar } from '../../components/Navbar/Navbar'
-import { Day } from '../../components/Day/Day'
 import { Template1 } from '../../components/Templates/Template1'
 import { Template2 } from '../../components/Templates/Template2'
 import { Template3 } from '../../components/Templates/Template3'
@@ -21,16 +20,16 @@ export const Holiday = () => {
 
     useEffect(() => {
         if (yearsList && yearsList.days) {
-            const daysArray = Object.values(yearsList.days);
-            setDaysList(daysArray);
+            const daysArray = Object.values(yearsList.days)
+            setDaysList(daysArray)
 
             if (daysArray.length === 0) {
-                navigate('/error404');
+                navigate('/error404')
             }
         } else {
-            navigate('/error404');
+            navigate('/error404')
         }
-    }, [year, yearsList, navigate]);
+    }, [year, yearsList, navigate])
 
     if (!daysList) {
         return <Error404 />
@@ -38,13 +37,30 @@ export const Holiday = () => {
 
     return (
         <>
-            <div className='red-cercle rotating_1'></div>
-            <svg className='blue-triangle rotating_2' width="797" height="838" viewBox="0 0 797 838" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M99.6363 832.288C47.4015 853.376 -7.72616 809.016 1.69682 753.479L119.299 60.3521C128.495 6.15103 192.591 -18.1632 235.422 16.3015L769.73 446.24C812.561 480.705 802.525 548.518 751.547 569.099L99.6363 832.288Z" fill="url(#paint0_linear_220_37)"/>
+            <div className="red-cercle rotating_1"></div>
+            <svg
+                className="blue-triangle rotating_2"
+                width="797"
+                height="838"
+                viewBox="0 0 797 838"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M99.6363 832.288C47.4015 853.376 -7.72616 809.016 1.69682 753.479L119.299 60.3521C128.495 6.15103 192.591 -18.1632 235.422 16.3015L769.73 446.24C812.561 480.705 802.525 548.518 751.547 569.099L99.6363 832.288Z"
+                    fill="url(#paint0_linear_220_37)"
+                />
                 <defs>
-                    <linearGradient id="paint0_linear_220_37" x1="244.994" y1="767.729" x2="439.159" y2="200.293" gradientUnits="userSpaceOnUse">
-                    <stop stopColor="#0996A5"/>
-                    <stop offset="1" stopColor="#D9D9D9" stopOpacity="0"/>
+                    <linearGradient
+                        id="paint0_linear_220_37"
+                        x1="244.994"
+                        y1="767.729"
+                        x2="439.159"
+                        y2="200.293"
+                        gradientUnits="userSpaceOnUse"
+                    >
+                        <stop stopColor="#0996A5" />
+                        <stop offset="1" stopColor="#D9D9D9" stopOpacity="0" />
                     </linearGradient>
                 </defs>
             </svg>
@@ -55,9 +71,10 @@ export const Holiday = () => {
             </header>
             <main className="holiday-main">
                 {daysList.map((day) => (
-                    <Day key={day.id} day={day}>
+                    <article key={day.id} className={'dayCard ' + day.template}>
+                        <h2 className={'dayCard__title'}>{day.title}</h2>
                         {getTemplateForDay(day)}
-                    </Day>
+                    </article>
                 ))}
             </main>
         </>
