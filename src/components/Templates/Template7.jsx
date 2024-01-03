a7ce833eadac5a433f9782ee3fe468924891856c
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-03 11:17:53 +0100

    Update Template7.jsx

--- src/components/Templates/Template7.jsx
+++ src/components/Templates/Template7.jsx
@@ -1,16 +1,24 @@
 export const Template7 = ({ day }) => {
-
-    const template = "template7";
+    const template = 'template7'
     return (
         <section key={day.id} className={template}>
-           <p className="title">{day.title}</p>
+            <p className="title">{day.title}</p>
             {day.pictures.map((picture, index) => (
-                <img key={"picture"+index} src={"/"+ picture.src} alt={picture.alt} className={"picture"+index} loading="lazy"/>
+                <img
+                    key={'picture' + index}
+                    src={'/' + picture.src}
+                    alt={picture.alt}
+                    className={'picture' + index}
+                    loading="lazy"
+                />
             ))}
-            {day.contents.map((content,index) => (
-                <p key={"txt"+index} className={"txt"+index} dangerouslySetInnerHTML={ {__html: content.text} }></p>
+            {day.contents.map((content, index) => (
+                <p
+                    key={'txt' + index}
+                    className={'txt' + index}
+                    dangerouslySetInnerHTML={{ __html: content.text }}
+                ></p>
             ))}
-            
         </section>
     )
 }
