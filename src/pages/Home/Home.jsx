44bb75cbb0978b04572e8193f8de10ddea52827b
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-03 20:06:35 +0100

    Adding useObserver and useRef

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
-import { Logo } from '../../components/Logo/Logo';
-import { Counter } from '../../components/Counter/Counter';
-import { BreakingNews } from '../../components/BreakingNews/BreakingNews';
-import { Cards } from '../../components/Cards/Cards';
-import { Description } from '../../components/Description/Description';
+import { useRef } from 'react'
+import { useObserver } from '../../utils/observer'
+import { Logo } from '../../components/Logo/Logo'
+import { Counter } from '../../components/Counter/Counter'
+import { BreakingNews } from '../../components/BreakingNews/BreakingNews'
+import { Cards } from '../../components/Cards/Cards'
+import { Description } from '../../components/Description/Description'
 
 export const Home = () => {
+    // Références aux éléments du DOM pour observer leur visibilité
+    const descriptionElementRef = useRef(null)
+    const counterElementRef = useRef(null)
+    const cardsElementRef = useRef(null)
+    const infoElementRef = useRef(null)
+
+    // Utilisation de useObserver pour observer la visibilité des éléments
+    const isDescriptionVisible = useObserver(descriptionElementRef, 0)
+    const isCounterVisible = useObserver(counterElementRef, 1)
+    const isCardsVisible = useObserver(cardsElementRef, 2)
+    const isInfoVisible = useObserver(infoElementRef, 3)
+
     return (
         <>
             <div className="red-cercle rotating_1"></div>
@@ -45,12 +59,32 @@ export const Home = () => {
                         Souvenirs d&apos;été <br />
                         <span>Les aventures fun-tastiques en famille</span>
                     </p>
-                    <Description content="presentation" />
-                    <Counter />
-                    <Cards />
-                    <Description content="informations" />
+                    <div
+                        ref={descriptionElementRef}
+                        className={`descriptionDiv ${isDescriptionVisible ? 'visible' : ''}`}
+                    >
+                        <Description content="presentation" />
+                    </div>
+                    <div
+                        ref={counterElementRef}
+                        className={`counterDiv ${isCounterVisible ? 'visible' : ''}`}
+                    >
+                        <Counter />
+                    </div>
+                    <div
+                        ref={cardsElementRef}
+                        className={`cardsDiv ${isCardsVisible ? 'visible' : ''}`}
+                    >
+                        <Cards />
+                    </div>
+                    <div
+                        ref={infoElementRef}
+                        className={`infoDiv ${isInfoVisible ? 'visible' : ''}`}
+                    >
+                        <Description content="informations" />
+                    </div>
                 </div>
             </main>
         </>
-    );
-};
+    )
+}
