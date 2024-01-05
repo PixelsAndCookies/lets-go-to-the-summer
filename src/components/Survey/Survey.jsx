c2acc222437ba8998ecd17fe4f670ad0d899f368
mroulland <morgane.roulland@hotmail.fr>
2024-01-05 10:47:49 +0100

    Amélioration sondage

--- src/components/Survey/Survey.jsx
+++ src/components/Survey/Survey.jsx
@@ -7,8 +7,8 @@ export const Survey = (props) => {
     const Name = props.name;
     const Json = surveyJson[Name];
     const Items = Json["items"];
-    const [sauceBarbecue, setSauceBarbecue] = useState((Items["barbecue"]*100)/Json["total"]);
-    const [sauceBearnaise, setSauceBearnaise] = useState((Items["bearnaise"]*100)/Json["total"]);
+    const [sauceBarbecue, setSauceBarbecue] = useState(Math.round((Items["barbecue"]*100)/Json["total"]));
+    const [sauceBearnaise, setSauceBearnaise] = useState(Math.round((Items["bearnaise"]*100)/Json["total"]));
     const [cookie, setCookie] = useState( () => 
     {
         if(document.cookie.length !== 0 && typeof document.cookie.split(";").find((row) => row.startsWith(Name)) !== "undefined")
@@ -43,18 +43,24 @@ export const Survey = (props) => {
     return (
         <form action="survey.php" method="get">
             <div>  
-                {!cookie && 
+                <div className="input">
+                    {!cookie && 
                     <input type="radio" value="0" id="barbecue" name={Name} onChange={(event) => handleChange(event)} /> 
-                }
-                <label htmlFor="barbecue">Team barbecue</label>
+                    }
+                    <label htmlFor="barbecue">Team barbecue <span>{sauceBarbecue}%</span></label>
+                </div>
                 <div className="progressBar"><div className="filler" style={{width: sauceBarbecue + '%'}}></div></div>
             </div>
             <div>
-                {!cookie && 
-                <input type="radio" value="1" id="bearnaise" name={Name} onChange={(event) => handleChange(event)}/>
-                }
-                 <label htmlFor="bearnaise">Team béarnaise</label>
-                <div className="progressBar"><div className="filler" style={{width: sauceBearnaise + '%'}}></div></div>
+                <div className="input">
+                    {!cookie && 
+                    <input type="radio" value="1" id="bearnaise" name={Name} onChange={(event) => handleChange(event)}/>
+                    }
+                    <label htmlFor="bearnaise">Team béarnaise <span>{Math.round(sauceBearnaise)}%</span></label>
+                </div>
+                <div className="progressBar">
+                    <div className="filler" style={{width: sauceBearnaise + '%'}}></div>
+                </div>
             </div>
         </form>
     );
