3b80bdb7ff72d3d864fb693aaa8312c795646928
LaurineRoulland <l.roulland@hotmail.fr>
2024-01-09 15:18:58 +0100

    Create Page Studio P&C

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home/Home.jsx'
-import { Holiday } from './pages/Holiday/Holiday.jsx'
-import { About } from './pages/About/About.jsx'
-import { Footer } from './components/Footer/Footer.jsx'
-import { Error404 } from './pages/Error404/Error404.jsx'
-import { Faq } from './pages/Faq/Faq.jsx'
-import { Licences } from './pages/Licences/Licences.jsx'
-import { Contact } from './pages/Contact/Contact.jsx'
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home/Home.jsx';
+import { Holiday } from './pages/Holiday/Holiday.jsx';
+import { About } from './pages/About/About.jsx';
+import { Footer } from './components/Footer/Footer.jsx';
+import { Error404 } from './pages/Error404/Error404.jsx';
+import { Faq } from './pages/Faq/Faq.jsx';
+import { Licences } from './pages/Licences/Licences.jsx';
+import { Contact } from './pages/Contact/Contact.jsx';
 import { MoumouPlayer } from './pages/MoumouPlayer/MoumouPlayer.jsx';
-import './style/main.scss'
+import { StudioPC } from './pages/SutdioP&C/StudioPC.jsx';
+import './style/main.scss';
 
 export default function App() {
     return (
@@ -21,6 +22,7 @@ export default function App() {
                 <Route path="/licences" element={<Licences />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/moumou" element={<MoumouPlayer />} />
+                <Route path="/studioPC" element={<StudioPC />} />
                 <Route path="*" element={<Error404 />} />
             </Routes>
             <Footer />
