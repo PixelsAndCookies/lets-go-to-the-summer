6a781a249c1fabbbaf2a0abed5b4477773ef4a20
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-17 15:43:55 +0100

    Remove Header components

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,23 +1,19 @@
-import { Header } from '../../components/Header/Header';
-
 export const About = () => {
     return (
         <>
-            <Header />
             <main className="main-container">
                 <article className="description otherPages">
                     <h2>A propos</h2>
                     <p style={{ lineHeight: 1.3 }}>
                         Quelques petites informations concernant ce site.
                         <br></br>
-                        Ce site est crée par la famille, pour la famille ! Par
-                        ailleurs, tout ce qui s&apos;y trouve est protégé et
-                        n&apos;est en aucun cas divulgué sur la toile. Afin de
-                        préserver notre intimité et continuer à partager nos
+                        Ce site est crée par la famille, pour la famille ! Par ailleurs, tout ce qui
+                        s&apos;y trouve est protégé et n&apos;est en aucun cas divulgué sur la
+                        toile. Afin de préserver notre intimité et continuer à partager nos
                         souvenirs et nos annecdotes en toute confiance.
                     </p>
                 </article>
             </main>
         </>
-    );
-};
+    )
+}
