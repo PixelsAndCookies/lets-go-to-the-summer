b7ef6f5fd86feb3085483e2da6a8ebab9ae6ff55
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-17 15:41:34 +0100

    Adding Header, create login and reworking route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { Routes, Route } from 'react-router-dom'
-import { GoToTopPage } from './utils/goToTopPage.jsx'
+// Import React
+import { useState } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+// Import JS
+import { LoadPageFromTop } from './utils/LoadPageFromTop.jsx'
+// Import Page
+import { SelectPrivacy } from './pages/SelectPrivacy/SelectPrivacy.jsx'
 import { Home } from './pages/Home/Home.jsx'
 import { Holiday } from './pages/Holiday/Holiday.jsx'
 import { About } from './pages/About/About.jsx'
@@ -10,25 +15,123 @@ import { Licences } from './pages/Licences/Licences.jsx'
 import { Contact } from './pages/Contact/Contact.jsx'
 import { MoumouPlayer } from './pages/MoumouPlayer/MoumouPlayer.jsx'
 import { StudioPC } from './pages/StudioP&C/StudioPC.jsx'
+// Import Composants
+import { Header } from './components/Header/Header.jsx'
+// Import Styles
 import './style/main.scss'
 
 export default function App() {
+    // Défini l'état d'authentification initial de l'utilisateur à false
+    const [isLogin, setIsLogin] = useState(false)
+    const location = useLocation()
+    // Défini si la page doit être publique ou privée
+    const isPrivate = location.state?.private || isLogin
+
+    // Défini l'état d'authentification à true lorsqu'elle est appelée
+    const handleLogin = async () => {
+        setIsLogin(true)
+    }
+
     return (
         <div className="App">
-            <GoToTopPage>
+            <LoadPageFromTop>
                 <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/holidays/:year" element={<Holiday />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/faq" element={<Faq />} />
-                    <Route path="/licences" element={<Licences />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/holidays/moumou" element={<MoumouPlayer />} />
-                    <Route path="/studioPC" element={<StudioPC />} />
-                    <Route path="*" element={<Error404 />} />
+                    <Route
+                        path="/"
+                        element={<SelectPrivacy isLogin={isPrivate} handleLogin={handleLogin} />}
+                    />
+                    <Route
+                        path="/home"
+                        element={
+                            <>
+                                <Header isHomePage={true} />
+                                <Home isLogin={isPrivate} />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/holidays/:year"
+                        element={
+                            <>
+                                <Header />
+                                <Holiday isLogin={isPrivate} />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/about"
+                        element={
+                            <>
+                                <Header />
+                                <About />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/faq"
+                        element={
+                            <>
+                                <Header />
+                                <Faq />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/licences"
+                        element={
+                            <>
+                                <Header />
+                                <Licences />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/contact"
+                        element={
+                            <>
+                                <Header />
+                                <Contact />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/holidays/moumou"
+                        element={
+                            <>
+                                <Header />
+                                <MoumouPlayer isLogin={isPrivate} />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/studioPC"
+                        element={
+                            <>
+                                <Header />
+                                <StudioPC />
+                                <Footer />
+                            </>
+                        }
+                    />
+                    <Route
+                        path="*"
+                        element={
+                            <>
+                                <Header />
+                                <Error404 />
+                                <Footer />
+                            </>
+                        }
+                    />
                 </Routes>
-                <Footer />
-            </GoToTopPage>
+            </LoadPageFromTop>
         </div>
     )
 }
