71decbfd53183c246fde042f29308c2e04dacab4
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-22 17:33:03 +0100

    Adding returnIcon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
+// Import React
+import { Link } from 'react-router-dom'
+// Import Composants
 import { Logo } from '../../components/Logo/Logo'
 import { Navbar } from '../../components/Navbar/Navbar'
+import returnIcon from '../../../public/pictures/icons/logout.png'
 
 export const Header = ({ isHomePage }) => {
     return (
         <header className={isHomePage ? 'header' : 'header-nav'}>
+            {isHomePage && (
+                <Link to={'/'} className="header__returnToPrivacyChoice">
+                    <img
+                        src={returnIcon}
+                        title="Retourner à la page de selection"
+                        alt="icône returnIcon crée par fariha begum"
+                    />
+                </Link>
+            )}
             <Logo />
             {!isHomePage && <Navbar />}
         </header>
