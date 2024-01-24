f170119787c607fa8f29a25443d41671722197dc
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-24 17:36:09 +0100

    Adding Link to each figure

--- src/pages/StudioP&C/StudioPC.jsx
+++ src/pages/StudioP&C/StudioPC.jsx
@@ -8,16 +8,22 @@ export const StudioPC = () => {
 
                     <div className="figure-container">
                         <figure>
-                            <img src="/pictures/mo.jpg" alt="photo de Morgane" loading="lazy" />
-                            <figcaption>Morgane</figcaption>
+                            <a href="https://github.com/mroulland" title="Lien pour vous rendre sur le Github de Morgane Roulland">
+                                <img src="/pictures/mo.jpg" alt="photo de Morgane Roulland" loading="lazy" />
+                                <figcaption>Morgane</figcaption>
+                            </a>
                         </figure>
                         <figure>
-                            <img src="/pictures/lau.jpg" alt="photo de Laurine" loading="lazy" />
+                            <a href="https://github.com/LaurineRoulland" title="Lien pour vous rendre sur le Github de Laurine Roulland">
+                            <img src="/pictures/lau.jpg" alt="photo de Laurine Roulland" loading="lazy" />
                             <figcaption>Laurine</figcaption>
+                            </a>
                         </figure>
                         <figure>
-                            <img src="/pictures/ax.jpg" alt="photo de Axel" loading="lazy" />
+                            <a href="https://github.com/AxelPrt73" title="Lien pour vous rendre sur le Github d'Axel Perret">
+                            <img src="/pictures/ax.jpg" alt="photo d'Axel Perret" loading="lazy" />
                             <figcaption>Axel</figcaption>
+                            </a>
                         </figure>
                     </div>
                 </article>
