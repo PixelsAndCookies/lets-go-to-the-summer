a8fec248f6584d21467277f14dffba430637307a
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-24 15:16:57 +0100

    Update BreakingNews.jsx

--- src/components/BreakingNews/BreakingNews.jsx
+++ src/components/BreakingNews/BreakingNews.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import breakingNewsPublic from '../../data/public/breakingNews.json'
-import breakingNewsPrivate from '../../data/private/breakingNewsPrivate.json'
 
-export const BreakingNews = ({ isLogin }) => {
-    const breakingNews = isLogin ? breakingNewsPrivate : breakingNewsPublic
+export const BreakingNews = () => {
 
     return (
         <div className="news-container">
             <p className="news-categ">BREAKING NEWS</p>
             <div className="news-text-bg news-text">
                 <p>
-                    {breakingNews.map((news) => (
+                    {breakingNewsPublic.map((news) => (
                         <React.Fragment key={news.id}>
                             {' '}
                             <strong className="news-text-title">{news.title}</strong>
