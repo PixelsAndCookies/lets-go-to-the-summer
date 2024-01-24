6a432bce969324d654a9bfc8854dd0308324b928
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-24 15:17:16 +0100

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,31 +1,23 @@
 import { Link } from 'react-router-dom'
 
-export const Logo = ({ isLink = true }) => {
-    const content = (
-        <>
-            <span>
-                <span className="warp">
-                    <span className="warp__0">L</span>
-                    <span className="warp__1">e</span>
-                    <span className="warp__2">t</span>
-                    <span className="warp__3">&apos;</span>
-                    <span className="warp__4">s</span>
-                    <span className="warp__5"> </span>
-                    <span className="warp__6">g</span>
-                    <span className="warp__7">o</span>
+export const Logo = () => {
+    return (
+        <h1>
+            <Link to="/">
+                <span>
+                    <span className="warp">
+                        <span className="warp__0">L</span>
+                        <span className="warp__1">e</span>
+                        <span className="warp__2">t</span>
+                        <span className="warp__3">&apos;</span>
+                        <span className="warp__4">s</span>
+                        <span className="warp__5"> </span>
+                        <span className="warp__6">g</span>
+                        <span className="warp__7">o</span>
+                    </span>
                 </span>
-            </span>
-            <span>to the</span> <span>summer</span>
-        </>
-    )
-    return isLink ? (
-        <h1>
-            <Link to="/home">{content}</Link>
-        </h1>
-    ) : (
-        <h1>
-            {/*Lien volontairement vide pour conserver le style de Logo.scss*/}
-            <Link to="">{content}</Link>
+                <span>to the</span> <span>summer</span>
+            </Link>
         </h1>
     )
 }
