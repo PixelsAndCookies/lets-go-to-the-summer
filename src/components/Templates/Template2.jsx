a787228da917072c5b8e1f3c21f19a8ca577fcf4
Axel PERRET <109222569+AxelPrt73@users.noreply.github.com>
2024-01-25 14:26:08 +0100

    Update Template2.jsx

--- src/components/Templates/Template2.jsx
+++ src/components/Templates/Template2.jsx
@@ -1,34 +1,36 @@
-import { Survey } from "../Survey/survey";
+import { Survey } from '../Survey/survey'
 
 export const Template2 = ({ day }) => {
-    const template = "template2";
-
-
+    const template = 'template2'
 
     return (
         <section key={template} className={template}>
-            <div className={template+"__containerTxt"}>
+            <div className={template + '__containerTxt'}>
                 {day.contents.map((content) => (
-                <p key={content.id}
-                    className={template+"__containerTxt__text "+template+"__containerTxt__text--"+content.id}>
-                    {content.text}
-                </p>
+                    <p
+                        key={content.id}
+                        className={
+                            template +
+                            '__containerTxt__text ' +
+                            template +
+                            '__containerTxt__text--' +
+                            content.id
+                        }
+                    >
+                        {content.text}
+                    </p>
                 ))}
-                <Survey name="sauces"/>
+                <Survey name="sauces" />
             </div>
-            <div className={template+"__containerImg"}>
+            <div className={template + '__containerImg'}>
                 {day.pictures.map((picture) => (
-                    <figure key={picture.id}
-                        className={template+"__containerImg--"+picture.id}>
-                    <img
-                        src={"/"+picture.src}
-                        alt={picture.alt}
-                        loading="lazy"
-                    />
-                    <figcaption>{picture.legend}</figcaption></figure>
+                    <figure key={picture.id} className={template + '__containerImg--' + picture.id}>
+                        <img src={'/' + picture.src} alt={picture.alt} loading="lazy" />
+                        <figcaption>{picture.legend}</figcaption>
+                    </figure>
                 ))}
             </div>
-            {day.quotes !== null && <q className={template+"__quotes"}>{day.quotes}</q>}
+            {day.quotes !== null && <q className={template + '__quotes'}>{day.quotes}</q>}
         </section>
     )
 }
